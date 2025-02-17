196c70cf5c6e3fd1b162833d7b29841840ea1569
The R <57016118+vida1678@users.noreply.github.com>
2025-02-17 21:28:36 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'The R'
+
+description 'A simple script that removes speed gained from bumps/curbs'
+
+files {
+    'handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
